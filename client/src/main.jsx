dde01707cbef267b1abc6b17a2c9b32c36316220
SonaliDutta-Dev <sonalidutta45bonu@gmail.com>
2025-11-21 10:13:37 +0530

    UI changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-  import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from "@clerk/clerk-react";
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  console.error("❌ Clerk Publishable Key missing");
 }
 
-createRoot(document.getElementById("root")).render( <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-         <BrowserRouter>
-    <App />
-  </BrowserRouter>
-      </ClerkProvider>
+createRoot(document.getElementById("root")).render(
+<ClerkProvider
+  publishableKey={PUBLISHABLE_KEY}
+  appearance={{
+    layout: {
+      shimmer: false,
+    },
+    elements: {
+      rootBox: "clerk-container",
+      card: "clerk-card",
+      footer: "clerk-footer",
+      formButtonPrimary: "clerk-btn",
+    }
+  }}
+>
 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 );
